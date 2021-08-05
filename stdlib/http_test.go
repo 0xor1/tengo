ccfacb677e836b6bbe8573242bab8c5b60f01a3e
Daniel Robinson <daniel.robinson.open@gmail.com>
2021-08-05 16:54:39 +0100

    update tests

--- stdlib/http_test.go
+++ stdlib/http_test.go
@@ -9,20 +9,20 @@ import (
 )
 
 func TestHTTP(t *testing.T) {
-	run := func(method string, url string, headers string, body string, errMsg string) {
+	run := func(required string, opts string, errMsg string) {
 		script := tengo.NewScript([]byte(fmt.Sprintf(`
 http := import("http")
-res := http.do(%q, %q, %s, %s)
-`, method, url, headers, body)))
+res := http.do(%s%s)
+`, required, opts)))
 		script.SetImports(stdlib.GetModuleMap("http"))
-		
+
 		executed, err := script.Run()
 		if err != nil {
 			t.Error(err)
 		}
-		
+
 		res := executed.Get("res").Value()
-		
+
 		err, ok := res.(error)
 		if ok {
 			if err.Error() != errMsg {
@@ -33,19 +33,21 @@ res := http.do(%q, %q, %s, %s)
 		if !ok && errMsg != "" {
 			t.Errorf("missing expected error")
 		}
-		
+
 		check := func(name string, ok func(i interface{}) bool) {
 			if !ok(res.(map[string]interface{})[name]) {
 				t.Errorf("unexpected %s value", name)
 			}
 		}
-		
+
 		check("code", func(i interface{}) bool { v, ok := i.(int64); return ok && v == 200 })
 		check("status", func(i interface{}) bool { v, ok := i.(string); return ok && v == "200 OK" })
 		check("headers", func(i interface{}) bool { v, ok := i.(map[string]interface{}); return ok && len(v) > 0 })
 		check("body", func(i interface{}) bool { v, ok := i.([]byte); return ok && len(v) > 0 })
 	}
-	
-	run(`GET`, `https://avatars.githubusercontent.com/u/1291934?s=48&v=4`, `{dnt: 1}`, `bytes("yolo")`, ``)
-	run(`GET`, `tengo`, `{dnt: 1}`, `bytes("yolo")`, `error: "Get \"tengo\": unsupported protocol scheme \"\""`)
+
+	required := `"GET", "https://avatars.githubusercontent.com/u/1291934?s=48&v=4"`
+	run(required, `, {dnt: 1, "my-header": "yolo"}, bytes("test")`, ``)
+	run(required, ``, ``)
+	run(`"GET", "tengo"`, ``, `error: "Get \"tengo\": unsupported protocol scheme \"\""`)
 }
